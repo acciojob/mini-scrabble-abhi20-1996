4d92995559ca8d32a794aca174dbbc0a4be77987
abhi20-1996 <110686880+abhi20-1996@users.noreply.github.com>
2023-06-07 23:49:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+// //your code here
+// let inPut= document.getElementById("evaluatedText");
+// let count= document.getElementById("letterCount");
+// inPut.addEventListener('input',()=>{
+// 	let str=inPut.value;
+// 	count.innerHTML=`${str.length}`;
+// })
 //your code here
-let inPut= document.getElementById("evaluatedText");
-let count= document.getElementById("letterCount");
-inPut.addEventListener('input',()=>{
-	let str=inPut.value;
-	count.innerHTML=`${str.length}`;
+let a= document.getElementById("evaluatedText");
+let b= document.getElementById("letterCount");
+
+a.addEventListener('input', ()=> {
+	let str= a.value;
+	b.innerHTML= `${str.length}`;
 })
